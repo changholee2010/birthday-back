26d8d7b70fa75a2419f3def78b23169affec7772
linuxie <linuxie@home.com>
2026-02-11 14:49:52 +0900

    for test

--- db_test.js
+++ db_test.js
@@ -1,10 +1,14 @@
 const getConnection = require("./db");
 
-let conn;
-conn = await getConnection();
-const result = await conn.execute(
-  `SELECT WRITER, CONTENT FROM CHURCH_MESSAGES ORDER BY REG_DATE DESC`,
-  {},
-  { outFormat: oracledb.OUT_FORMAT_OBJECT },
-);
-console.log(result.rows);
+async function main() {
+  let conn;
+  conn = await getConnection();
+  const result = await conn.execute(
+    `SELECT WRITER, CONTENT FROM CHURCH_MESSAGES ORDER BY REG_DATE DESC`,
+    {},
+    { outFormat: oracledb.OUT_FORMAT_OBJECT },
+  );
+
+  console.log(result.rows);
+}
+main();
