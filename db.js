1991d84325ccec0f84280b776f4df9197b50bde7
linuxie <linuxie@home.com>
2026-02-11 16:44:15 +0900

    done

--- db.js
+++ db.js
@@ -1,6 +1,12 @@
 const oracledb = require("oracledb");
 
 // oracledb.initOracleClient({ libDir: "/usr/lib/oracle" });
+oracledb.initOracleClient({
+  // Instant Client 라이브러리 위치
+  libDir: "/opt/oracle/instantclient_21_21",
+  // tnsnames.ora, sqlnet.ora가 있는 Wallet 위치
+  configDir: "/home/ubuntu/wallet",
+});
 
 async function getConnection() {
   return await oracledb.getConnection({
