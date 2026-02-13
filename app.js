718808f5c9c40389da1a8fa09e74cf9b12997430
you <you@email.com>
2026-02-13 12:42:23 +0900

    CI/CD test 3

--- app.js
+++ app.js
@@ -45,4 +45,4 @@ app.get("/api/messages", async (req, res) => {
   }
 });
 
-app.listen(3000, () => console.log("Server running on port 3000"));
+app.listen(3000, "0.0.0.0", () => console.log("Server running on port 3000"));
